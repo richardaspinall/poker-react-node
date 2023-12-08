5eafb743fcac612591da4f72e9cf50e2d94728a4
Richard Aspinall <rls.aspinall@gmail.com>
2023-12-09 09:31:11 +1100

    break tests and remove specific version of node

--- backend/src/tests/HelloWorld.spec.ts
+++ backend/src/tests/HelloWorld.spec.ts
@@ -2,6 +2,6 @@ import HelloWorld from './HelloWorld';
 
 describe('HelloWorld', () => {
   it('should return "Hello World"', () => {
-    expect(HelloWorld()).toBe('Hello World');
+    expect(HelloWorld()).toBe('Hello WorldS');
   });
 });
