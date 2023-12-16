e5b332818dc9ee45ae49f3e23dbc77b0a26fd520
Richard Aspinall <rls.aspinall@gmail.com>
2023-12-17 12:59:42 +1300

    add endpoint tests via supertest

---
+++ backend/src/routes/actions.test.ts
@@ -0,0 +1,70 @@
+// external modules
+import request from 'supertest';
+
+// internal modules
+import httpServer, { shutDown } from '../index';
+import Logger from '../utils/Logger';
+
+// mocks (not all used here, but leaving cos it could be in future)
+import {
+  mockJoinRoomSuccess,
+  mockJoinRoomError,
+  mockSendEventToRoomSuccess,
+  mockSendEventToRoomError,
+} from './roomMocks';
+
+import Rooms from '../sockets/Rooms';
+import Result, { ResultSuccess, ResultError } from '../shared/Result';
+
+const debug = Logger.newDebugger('test:tables');
+
+describe('tables.join', () => {
+  // TODO: will eventually need to add the table they are sitting at but this is hardcoded
+  // for now. See third commented out test
+  it('should seat a player to a table', async () => {
+    const res = await request(httpServer).post('/api/actions/tables.join').send({
+      selectedSeatNumber: 'seat-1',
+      socketId: 'abc123',
+    });
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.ok).toEqual(true);
+  });
+
+  it('should error when the seat is already taken', async () => {
+    jest.spyOn(Rooms, 'sendEventToRoom').mockImplementation(() => new ResultError('Room not found'));
+    await request(httpServer).post('/api/actions/tables.join').send({
+      selectedSeatNumber: 'seat-1',
+      socketId: 'abc123',
+    });
+    const res = await request(httpServer).post('/api/actions/tables.join').send({
+      selectedSeatNumber: 'seat-1',
+      socketId: 'def456',
+    });
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.ok).toEqual(false);
+    expect(res.body.error).toEqual('Seat is already taken');
+  });
+
+  // it('should error when the table doesnt exist', async () => {
+  //   mockSendEventToRoomError();
+  //   const res = await request(httpServer).post('/api/actions/tables.join').send({
+  //     table: 'table_doesnt_exist',
+  //     selectedSeatNumber: 'seat-1',
+  //     socketId: 'abc123',
+  //   });
+
+  //   expect(res.statusCode).toEqual(200);
+  //   expect(res.body.ok).toEqual(false);
+  //   expect(res.body.error).toEqual('Room not found');
+  // });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  // Shut down the server after all tests
+  afterAll(async () => {
+    await shutDown();
+  });
+});
