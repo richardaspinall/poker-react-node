aba97744731ceddb3ce5fd191d68c419d7c3a042
Thomas Frazer <thomasmichaelfrazer@gmail.com>
2024-01-04 14:26:50 +1100

    Fix bug with looping through all seats and checking if user is already sitting at table

--- backend/src/game/PokerTable.ts
+++ backend/src/game/PokerTable.ts
@@ -51,6 +51,8 @@ export default class PokerTable {
             if (seat.playerId == clientId ){
                 return Result.error('Player is already sitting at the table');
             }
+        }
+        for (const seat of table.seats) {
             // Check if the seat is already taken by someone else
             if (seat.seatNumber == seatNumber) {
                 if (seat.isTaken){
