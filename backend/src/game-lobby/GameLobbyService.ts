0dd1025fd323597c912aaff8743e9a99b07ba40c
Richard Aspinall <rls.aspinall@gmail.com>
2024-01-06 11:50:38 +1100

    add game lobby and table

---
+++ backend/src/game-lobby/GameLobbyService.ts
@@ -0,0 +1,33 @@
+// internal modules
+import GameLobby from './GameLobby';
+import PokerTable from './PokerTable';
+import Result from '../shared/Result';
+
+export class GameLobbyService {
+  private gameLobby: GameLobby;
+
+  constructor() {
+    this.gameLobby = new GameLobby();
+
+    // seed a table until we have a database
+    this.createPokerTable('table_1', 2);
+  }
+
+  public createPokerTable(name: string, numSeats: number): Result<void> {
+    // do some checking on name through the GameLobby
+    if (this.gameLobby.isNameTaken(name)) {
+      return Result.error('name_taken');
+    }
+
+    const pokerTable = new PokerTable(name, numSeats);
+    this.gameLobby.addTable(pokerTable);
+
+    return Result.success();
+  }
+
+  public getTable(name: string): PokerTable | undefined {
+    return this.gameLobby.getTables().find((table) => table.getName() === name);
+  }
+}
+
+export default new GameLobbyService();
