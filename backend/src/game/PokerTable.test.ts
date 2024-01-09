4f10d54c708e134c7384b8374488d3a138db2dc5
tommy trombone <thomasmichaelfrazer@gmail.com>
2024-01-10 10:08:53 +1100

    Unit test sitAtTable function (#34)
    * sitAtTable unit tests
    
    * Make table unique for each unit test
    
    * Add empty message check for successful test
    
    * Update comment and naming convention

---
+++ backend/src/game/PokerTable.test.ts
@@ -0,0 +1,57 @@
+import PokerTable from './PokerTable';
+import Result from '../shared/Result';
+import Rooms from '../sockets/Rooms';
+
+describe('sitAtTable', () => {
+  it('should confirm a player has sat down', () => {
+    const tableName = 'table_1';
+    PokerTable.createPokerTable(tableName, 2);
+    jest.spyOn(Rooms, 'sendEventToRoom').mockImplementation(() => Result.success());
+    const res = PokerTable.sitAtTable(tableName, 'seat-1', 'a1')
+
+    expect(res.ok).toEqual(true);
+    expect(res.isError).toEqual(false);
+    expect(res.errorMessage).toEqual('');
+  });
+
+  it('should error when seat is already taken', () => {
+    const tableName = 'table_2';
+    PokerTable.createPokerTable(tableName, 2);
+    jest.spyOn(Rooms, 'sendEventToRoom').mockImplementation(() => Result.success());
+    PokerTable.sitAtTable(tableName, 'seat-1', 'a1')
+    jest.spyOn(Rooms, 'sendEventToRoom').mockImplementation(() => Result.success());
+    const res = PokerTable.sitAtTable(tableName, 'seat-1', 'b2')
+    
+    expect(res.ok).toEqual(false);
+    expect(res.isError).toEqual(true);
+    expect(res.errorMessage).toEqual('Seat is already taken');
+  });
+
+  it('should error when player is already sitting down', () => {
+    const tableName = 'table_3';
+    PokerTable.createPokerTable(tableName, 2);
+    jest.spyOn(Rooms, 'sendEventToRoom').mockImplementation(() => Result.success());
+    PokerTable.sitAtTable(tableName, 'seat-1', 'a1')
+    jest.spyOn(Rooms, 'sendEventToRoom').mockImplementation(() => Result.success());
+    const res = PokerTable.sitAtTable(tableName, 'seat-2', 'a1')
+
+    expect(res.ok).toEqual(false);
+    expect(res.isError).toEqual(true);
+    expect(res.errorMessage).toEqual('Player is already sitting at the table');
+  });
+
+  it('should error when seat number does not exist', () => {
+    const tableName = 'table_4';
+    PokerTable.createPokerTable(tableName, 2);
+    jest.spyOn(Rooms, 'sendEventToRoom').mockImplementation(() => Result.success());
+    const res = PokerTable.sitAtTable(tableName, 'seat-3', 'a1')
+
+    expect(res.ok).toEqual(false);
+    expect(res.isError).toEqual(true);
+    expect(res.errorMessage).toEqual('Seat not found');
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+});
