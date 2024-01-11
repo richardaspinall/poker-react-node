7c0c1d7adcc7e6e20637bdf93bbac92cb4b140ae
Richard Aspinall <rls.aspinall@gmail.com>
2024-01-12 11:03:25 +1300

    feat: set up mysql (#19)
    * add script for creating and seeing db
    
    * add instructions and command to run script
    
    * update package information

---
+++ backend/db/seed.sql
@@ -0,0 +1,11 @@
+INSERT INTO user
+(username, password)
+VALUES 
+('raspinall', 'testpassword'),
+('thomas', 'testpassword');
+
+INSERT INTO pokertable
+(name, data)
+VALUES
+('table_1', '{"key1": "value1", "key2": "value2"}'),
+('table_2','{"key1": "value1", "key2": "value2"}');
