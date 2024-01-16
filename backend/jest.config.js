b70e9f62c32986218b41dd19c1be4b1369996c93
Richard Aspinall <rls.aspinall@gmail.com>
2024-01-16 23:50:47 +1100

    separate tests for db as test suite cant run

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
     '^.+\\.ts?$': 'ts-jest',
   },
   testEnvironment: 'node',
-  testRegex: '(./src/|./db/).*\\.(test|spec)?\\.(ts|ts)$',
+  testRegex: './src/.*\\.(test|spec)?\\.(ts|ts)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   modulePathIgnorePatterns: ['<rootDir>/src/handlers'],
   roots: ['<rootDir>/src'],
