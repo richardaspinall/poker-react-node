37d7a51a7e4dfd6771dc8fc986a12deb5824eda0
Richard Aspinall <rls.aspinall@gmail.com>
2024-02-06 08:51:38 +1100

    refactor: make generic

--- backend/src/shared/BaseHandler.ts
+++ backend/src/shared/BaseHandler.ts
@@ -1,8 +1,24 @@
+// External
 import { Request, Response } from 'express';
+import Joi from 'joi';
 
-export default abstract class BaseHandler<T> {
-  public runHandler(req: Request, res: Response<T>) {
-    return this.getResult(req, res);
+// Internal
+import { BaseOutput } from './api/types/BaseOutput';
+import { validatePayload } from './validatePayload';
+import Result from './Result';
+
+export default abstract class BaseHandler<TInput> {
+  constructor(private validationSchema: Joi.ObjectSchema<any>) {}
+
+  public runHandler(req: Request<TInput>, res: Response<BaseOutput>) {
+    const payload = validatePayload<TInput>(this.validationSchema, req.body);
+
+    if (payload.isError) {
+      res.status(400).send({ ok: false, error: payload.errorMessage, error_details: payload.errorDetails });
+      return;
+    }
+
+    return this.getResult(payload, res);
   }
-  protected abstract getResult(req: Request, res: Response<T>): any;
+  protected abstract getResult(payload: Result<TInput>, res: Response<BaseOutput>): any;
 }
---
+++ backend/src/shared/api/types/BaseOutput.ts
@@ -0,0 +1,6 @@
+export type BaseOutput = {
+  ok: boolean;
+  payload?: any;
+  error?: string;
+  error_details?: string;
+};
