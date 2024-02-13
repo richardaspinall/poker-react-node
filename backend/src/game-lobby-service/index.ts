1fbe5f0b5ebfb7b018e54d273cc3baaf7c5f1cfe
Richard Aspinall <rls.aspinall@gmail.com>
2024-02-14 09:50:39 +1100

    add error test request

--- backend/src/game-lobby-service/index.ts
+++ backend/src/game-lobby-service/index.ts
@@ -2,6 +2,5 @@ import GameLobbyService from './GameLobbyService';
 
 // TODO: Seed / hardcode for now until we are working with a DB
 const gameLobbyService = new GameLobbyService();
-console.log('here');
 gameLobbyService.createPokerTable('table_1', 2);
 export default gameLobbyService;
