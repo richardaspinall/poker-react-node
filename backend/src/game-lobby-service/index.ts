1b7b927e86aa14059db9c63d9c2261f065075ceb
Richard Aspinall <rls.aspinall@gmail.com>
2024-02-14 09:40:38 +1100

    fix up requests

--- backend/src/game-lobby-service/index.ts
+++ backend/src/game-lobby-service/index.ts
@@ -1,5 +1,6 @@
 import GameLobbyService from './GameLobbyService';
-// Seed for now
+
+// TODO: Seed / hardcode for now until we are working with a DB
 const gameLobbyService = new GameLobbyService();
 console.log('here');
 gameLobbyService.createPokerTable('table_1', 2);
