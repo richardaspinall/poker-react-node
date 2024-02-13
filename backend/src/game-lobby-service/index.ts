449cbbe1b12ddaef71783e0b32d91b7745fca83a
Richard Aspinall <rls.aspinall@gmail.com>
2024-02-14 09:27:23 +1100

    add test requests

--- backend/src/game-lobby-service/index.ts
+++ backend/src/game-lobby-service/index.ts
@@ -1,5 +1,6 @@
 import GameLobbyService from './GameLobbyService';
 // Seed for now
 const gameLobbyService = new GameLobbyService();
+console.log('here');
 gameLobbyService.createPokerTable('table_1', 2);
 export default gameLobbyService;
