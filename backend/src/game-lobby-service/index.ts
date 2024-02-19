07e5c814b1d4113b0683829c297e049bf9a15867
Richard Aspinall <rls.aspinall@gmail.com>
2024-02-19 20:33:03 +1100

    remove new instance of GameLobbyService

--- backend/src/game-lobby-service/index.ts
+++ backend/src/game-lobby-service/index.ts
@@ -2,4 +2,4 @@ import GameLobbyService from './GameLobbyService';
 // Seed for now
 const gameLobbyService = new GameLobbyService();
 gameLobbyService.createPokerTable('table_1', 2);
-export default new GameLobbyService();
+export default gameLobbyService;
