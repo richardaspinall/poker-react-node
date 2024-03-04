1e6a5b22f1e9abdcc3beb48ff21a744b5f9000d7
Richard Aspinall <rls.aspinall@gmail.com>
2024-03-05 08:16:21 +1100

    update imports

--- backend/src/tests/mocks/dbMocks.ts
+++ backend/src/tests/mocks/dbMocks.ts
@@ -1,11 +1,9 @@
-// Internal
-import { MySqLInstance } from '../../db/MySql';
-
 // External
 import { RowDataPacket } from 'mysql2/promise';
 
 // Internal
 import { Result, ResultSuccess, ResultError } from '@shared/Result';
+import { MySqLInstance } from '../../db/MySql';
 import { DBInsertError, DBInsertDuplicateError } from '@shared/errors/DB/DBInsertErrors';
 import { DBSelectError } from '@shared/errors/DB/DBSelectErrors';
 
