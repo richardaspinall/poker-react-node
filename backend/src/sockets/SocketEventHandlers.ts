5b180041f76b69b14d5d6f850510adccefd159f3
Richard Aspinall <rls.aspinall@gmail.com>
2024-03-08 08:51:37 +1100

    fix: add types to sockethandlers

--- backend/src/sockets/SocketEventHandlers.ts
+++ backend/src/sockets/SocketEventHandlers.ts
@@ -3,6 +3,7 @@ import { Socket } from 'socket.io';
 
 // Internal utils
 import { Logger } from '../utils/Logger';
+import { ClientToServerEvents, ServerToClientEvents } from './SocketEvents';
 
 const debug = Logger.newDebugger('APP:SocketEventHandlers');
 
@@ -10,7 +11,7 @@ const debug = Logger.newDebugger('APP:SocketEventHandlers');
  * SocketHandlers is responsible for handling socket events
  */
 export class SocketHandlers {
-  static setUpHandlers(socket: Socket) {
+  static setUpHandlers(socket: Socket<ClientToServerEvents, ServerToClientEvents>) {
     // response is a callback function that can be used to send data back to the client
     socket.on('hello_from_client', (value: string, response) => {
       debug(value);
@@ -20,10 +21,6 @@ export class SocketHandlers {
     socket.on('disconnect', (reason) => {
       debug('disconnected');
       // Handle disconnection logic if needed
-    });
-
-    socket.on('some_event', (payload) => {
-      // Handle 'some_event' logic
     });
   }
 }
