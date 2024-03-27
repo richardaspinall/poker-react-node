c0c4e3f2e8eba7c48b8c9aaa420a29495dba8c65
Richard Aspinall <rls.aspinall@gmail.com>
2024-03-28 09:29:52 +1100

    add sessions to handlers

--- backend/src/handlers/signin/SigninHandler.ts
+++ backend/src/handlers/signin/SigninHandler.ts
@@ -1,14 +1,12 @@
 import express from 'express';
-import type { Request, Response } from 'express';
-
-import { Result } from '@infra/Result';
+import type { NextFunction, Request, Response } from 'express';
 
 import { SigninErrorCodes } from '@shared/signin/types/Signin';
 import { UserService } from '../../users/UserService';
-import { UserSessionStore } from '../../users/UserSessionStore';
 import { ErrorHandler } from '../BaseHandler';
 
 import { validatePayload } from '../validatePayload';
+import { mapBaseErrorToAPIError } from '../helpers/mapBaseErrorToAPIError';
 
 import { type SigninPayload, type SigninOutput, signinSchema } from '../../shared/signin/types/Signin';
 
@@ -17,27 +15,36 @@ export const router = express.Router();
  * SigninHandler signs in a user
  */
 class SigninHandler {
-  public async getResult(req: Request<SigninPayload>, res: Response<SigninOutput>) {
-    const payload = validatePayload<SigninPayload>(signinSchema, req.body);
-    console.log(req.session);
-    console.log(req.session.id);
-    const username = payload.getValue().username;
-    const password = payload.getValue().password;
+  public async getResult(req: Request<SigninPayload>, res: Response<SigninOutput>, next: NextFunction) {
+    try {
+      const payload = validatePayload<SigninPayload>(signinSchema, req.body);
 
-    const passwordIsValid = await UserService.validatePassword(username, password);
+      if (payload.isError()) {
+        const error = payload.getError();
+        const apiError = mapBaseErrorToAPIError(error);
 
-    if (passwordIsValid.isError()) {
-      return ErrorHandler.handleError(passwordIsValid.getError(), SigninErrorCodes, res);
+        res.status(400).send({ ok: false, error: apiError });
+        return;
+      }
+
+      const username = payload.getValue().username;
+      const password = payload.getValue().password;
+      const passwordIsValid = await UserService.validatePassword(username, password);
+
+      if (passwordIsValid.isError()) {
+        return ErrorHandler.handleError(passwordIsValid.getError(), SigninErrorCodes, res);
+      }
+      if (req.session.authenticated) {
+        console.log('User already authenticated');
+      } else {
+        req.session.username = username;
+        req.session.authenticated = true;
+      }
+
+      return res.send({ ok: true });
+    } catch (error) {
+      next(error); // Pass any caught errors to next() for error handling
     }
-
-    UserSessionStore.addUserSession(req.session.id, username);
-
-    req.session.username = username;
-    req.session.authenticated = true;
-    req.session.save();
-    console.log(req.session);
-
-    return res.send({ ok: true });
   }
 }
 
