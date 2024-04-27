db1f2924a0cbebb618c4beda2c5ce172f2dfd45e
Richard Aspinall <rls.aspinall@gmail.com>
2024-04-27 13:25:17 +1000

    re rename

--- backend/src/infra/routes/routeConfig.ts
+++ backend/src/infra/routes/routeConfig.ts
@@ -1,5 +1,5 @@
 import { APIMethodShape } from '../../shared/api/APIMethodShape';
-import { APIMethods } from '../../shared/api/APIMethodsNEW';
+import { APIMethods } from '../../shared/api/APIMethods';
 
 export const routes: APIMethodShape[] = Object.keys(APIMethods).map((key) => {
   const apiMethod = APIMethods[key as keyof typeof APIMethods];
