fa95c0fbca217c209a502f98d6fe3d3b75b0e117
Richard Aspinall <rls.aspinall@gmail.com>
2024-04-29 21:01:06 +1000

    delete files

--- backend/src/handlers/users/errors/UsernameNotFoundError.ts
+++
@@ -1,7 +0,0 @@
-import { BaseError } from '@infra/BaseError';
-
-export class UsernameNotFoundError extends BaseError {
-  constructor() {
-    super('username_not_found', 'Username not found');
-  }
-}
