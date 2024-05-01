45b3d4ee6bacb16fd23a09a8904bcfec57d21fd5
Richard Aspinall <rls.aspinall@gmail.com>
2024-05-01 10:43:09 +1000

    feat: add images for cards (#120)
    * add svgs
    
    * move enum
    
    * add facedown cards

---
+++ frontend/src/components/Card/Card.tsx
@@ -0,0 +1,16 @@
+import { CardCode } from './CardCode';
+import { cardMap } from './cardMap';
+
+interface CardProps {
+  cardCode: CardCode;
+}
+
+export function Card({ cardCode }: Readonly<CardProps>) {
+  const CardSVG = cardMap[cardCode];
+
+  return (
+    <div className="card">
+      {CardSVG ? <img src={CardSVG} className="" alt="Facedown cards" /> : <p>Card not found</p>}
+    </div>
+  );
+}
---
+++ frontend/src/components/Card/card-svgs/index.ts
@@ -0,0 +1,111 @@
+import TwoOfClubs from '../../../assets/cards/2C.svg';
+import TwoOfDiamonds from '../../../assets/cards/2D.svg';
+import TwoOfHearts from '../../../assets/cards/2H.svg';
+import TwoOfSpades from '../../../assets/cards/2S.svg';
+import ThreeOfClubs from '../../../assets/cards/3C.svg';
+import ThreeOfDiamonds from '../../../assets/cards/3D.svg';
+import ThreeOfHearts from '../../../assets/cards/3H.svg';
+import ThreeOfSpades from '../../../assets/cards/3S.svg';
+import FourOfClubs from '../../../assets/cards/4C.svg';
+import FourOfDiamonds from '../../../assets/cards/4D.svg';
+import FourOfHearts from '../../../assets/cards/4H.svg';
+import FourOfSpades from '../../../assets/cards/4S.svg';
+import FiveOfClubs from '../../../assets/cards/5C.svg';
+import FiveOfDiamonds from '../../../assets/cards/5D.svg';
+import FiveOfHearts from '../../../assets/cards/5H.svg';
+import FiveOfSpades from '../../../assets/cards/5S.svg';
+import SixOfClubs from '../../../assets/cards/6C.svg';
+import SixOfDiamonds from '../../../assets/cards/6D.svg';
+import SixOfHearts from '../../../assets/cards/6H.svg';
+import SixOfSpades from '../../../assets/cards/6S.svg';
+import SevenOfClubs from '../../../assets/cards/7C.svg';
+import SevenOfDiamonds from '../../../assets/cards/7D.svg';
+import SevenOfHearts from '../../../assets/cards/7H.svg';
+import SevenOfSpades from '../../../assets/cards/7S.svg';
+import EightOfClubs from '../../../assets/cards/8C.svg';
+import EightOfDiamonds from '../../../assets/cards/8D.svg';
+import EightOfHearts from '../../../assets/cards/8H.svg';
+import EightOfSpades from '../../../assets/cards/8S.svg';
+import NineOfClubs from '../../../assets/cards/9C.svg';
+import NineOfDiamonds from '../../../assets/cards/9D.svg';
+import NineOfHearts from '../../../assets/cards/9H.svg';
+import NineOfSpades from '../../../assets/cards/9S.svg';
+import TenOfClubs from '../../../assets/cards/10C.svg';
+import TenOfDiamonds from '../../../assets/cards/10D.svg';
+import TenOfHearts from '../../../assets/cards/10H.svg';
+import TenOfSpades from '../../../assets/cards/10S.svg';
+import AceOfClubs from '../../../assets/cards/AC.svg';
+import AceOfDiamonds from '../../../assets/cards/AD.svg';
+import AceOfHearts from '../../../assets/cards/AH.svg';
+import AceOfSpades from '../../../assets/cards/AS.svg';
+import FaceDownCard from '../../../assets/cards/FacedownCard.svg';
+import FaceDownCards from '../../../assets/cards/FacedownCards.svg';
+import JackOfClubs from '../../../assets/cards/JC.svg';
+import JackOfDiamonds from '../../../assets/cards/JD.svg';
+import JackOfHearts from '../../../assets/cards/JH.svg';
+import JackOfSpades from '../../../assets/cards/JS.svg';
+import KingOfClubs from '../../../assets/cards/KC.svg';
+import KingOfDiamonds from '../../../assets/cards/KD.svg';
+import KingOfHearts from '../../../assets/cards/KH.svg';
+import KingOfSpades from '../../../assets/cards/KS.svg';
+import QueenOfClubs from '../../../assets/cards/QC.svg';
+import QueenOfDiamonds from '../../../assets/cards/QD.svg';
+import QueenOfHearts from '../../../assets/cards/QH.svg';
+import QueenOfSpades from '../../../assets/cards/QS.svg';
+
+export {
+  TwoOfClubs,
+  TwoOfDiamonds,
+  TwoOfHearts,
+  TwoOfSpades,
+  ThreeOfClubs,
+  ThreeOfDiamonds,
+  ThreeOfHearts,
+  ThreeOfSpades,
+  FourOfClubs,
+  FourOfDiamonds,
+  FourOfHearts,
+  FourOfSpades,
+  FiveOfClubs,
+  FiveOfDiamonds,
+  FiveOfHearts,
+  FiveOfSpades,
+  SixOfClubs,
+  SixOfDiamonds,
+  SixOfHearts,
+  SixOfSpades,
+  SevenOfClubs,
+  SevenOfDiamonds,
+  SevenOfHearts,
+  SevenOfSpades,
+  EightOfClubs,
+  EightOfDiamonds,
+  EightOfHearts,
+  EightOfSpades,
+  NineOfClubs,
+  NineOfDiamonds,
+  NineOfHearts,
+  NineOfSpades,
+  TenOfClubs,
+  TenOfDiamonds,
+  TenOfHearts,
+  TenOfSpades,
+  AceOfClubs,
+  AceOfDiamonds,
+  AceOfHearts,
+  AceOfSpades,
+  JackOfClubs,
+  JackOfDiamonds,
+  JackOfHearts,
+  JackOfSpades,
+  KingOfClubs,
+  KingOfDiamonds,
+  KingOfHearts,
+  KingOfSpades,
+  QueenOfClubs,
+  QueenOfDiamonds,
+  QueenOfHearts,
+  QueenOfSpades,
+  FaceDownCards,
+  FaceDownCard,
+};
