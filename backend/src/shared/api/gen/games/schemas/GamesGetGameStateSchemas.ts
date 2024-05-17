ac9b76a0d825e8eeac92f5c5b5036d3dd047d140
Richard Aspinall <rls.aspinall@gmail.com>
2024-05-17 22:05:15 +1000

    use payload

--- backend/src/shared/api/gen/games/schemas/GamesGetGameStateSchemas.ts
+++ backend/src/shared/api/gen/games/schemas/GamesGetGameStateSchemas.ts
@@ -7,21 +7,23 @@ export const GamesGetGameStatePayloadSchema = Joi.object<GamesGetGameStatePayloa
 }).unknown(false);
 
 export const GamesGetGameStateOutputSchema = Joi.object<GamesGetGameStateOutput>({
-  bigBlind: Joi.number().required(),
-  communityCards: Joi.array()
-    .items(
-      Joi.object({
-        cardShortCode: Joi.string().required(),
-        rank: Joi.string().required(),
-        suit: Joi.string().required(),
-      }),
-    )
-    .required(),
-  currentBet: Joi.number().required(),
-  dealerPosition: Joi.number().required(),
   ok: Joi.boolean().required(),
-  pot: Joi.number().required(),
-  roundState: Joi.string().required(),
-  seatToAct: Joi.number().required(),
-  smallBlind: Joi.number().required(),
+  payload: Joi.object({
+    bigBlind: Joi.number().required(),
+    communityCards: Joi.array()
+      .items(
+        Joi.object({
+          cardShortCode: Joi.string().required(),
+          rank: Joi.string().required(),
+          suit: Joi.string().required(),
+        }),
+      )
+      .required(),
+    currentBet: Joi.number().required(),
+    dealerPosition: Joi.number().required(),
+    pot: Joi.number().required(),
+    roundState: Joi.string().required(),
+    seatToAct: Joi.number().required(),
+    smallBlind: Joi.number().required(),
+  }).required(),
 }).unknown(false);
--- backend/src/shared/api/gen/games/types/GamesGetGameState.ts
+++ backend/src/shared/api/gen/games/types/GamesGetGameState.ts
@@ -5,19 +5,21 @@ export interface GamesGetGameStatePayload {
 }
 
 export interface GamesGetGameStateOutput extends BaseOutput {
-  bigBlind: number;
-  communityCards: {
-    cardShortCode: string;
-    rank: string;
-    suit: string;
-  }[];
-  currentBet: number;
-  dealerPosition: number;
   ok: boolean;
-  pot: number;
-  roundState: string;
-  seatToAct: number;
-  smallBlind: number;
+  payload: {
+    bigBlind: number;
+    communityCards: {
+      cardShortCode: string;
+      rank: string;
+      suit: string;
+    }[];
+    currentBet: number;
+    dealerPosition: number;
+    pot: number;
+    roundState: string;
+    seatToAct: number;
+    smallBlind: number;
+  };
 }
 
 export enum GamesGetGameStateErrorCodes {
