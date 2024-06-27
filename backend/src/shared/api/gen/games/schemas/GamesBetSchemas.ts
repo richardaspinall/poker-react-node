b9e0c5d0ff815008e2e2c7694c3c0ba8dbb4e8da
Thomas Frazer <thomasmichaelfrazer@gmail.com>
2024-06-27 11:33:48 +1000

    Rerun generator

--- backend/src/shared/api/gen/games/schemas/GamesBetSchemas.ts
+++ backend/src/shared/api/gen/games/schemas/GamesBetSchemas.ts
@@ -3,7 +3,7 @@ import Joi from 'joi';
 import { GamesBetOutput, GamesBetPayload } from '../types/GamesBet';
 
 export const GamesBetPayloadSchema = Joi.object<GamesBetPayload>({
-  betAmount: Joi.number().required(),
+  amount: Joi.number().required(),
   pokerTableName: Joi.string().required(),
 }).unknown(false);
 
