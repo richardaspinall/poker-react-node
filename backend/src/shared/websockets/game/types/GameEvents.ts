f81f00335e9113e366b2009cb61ee91c131bfba6
Richard Aspinall <rls.aspinall@gmail.com>
2024-07-19 10:44:31 +1000

    add action events

--- backend/src/shared/websockets/game/types/GameEvents.ts
+++ backend/src/shared/websockets/game/types/GameEvents.ts
@@ -5,6 +5,10 @@ export enum GameEvent {
   DEAL_CARDS = 'deal_cards',
   FOLD_CARDS = 'fold_cards',
   SEAT_TO_ACT = 'seat_to_act',
+  DEAL_COMMUNITY_CARDS = 'deal_community_cards',
+  UPDATE_POT = 'update_pot',
+  PLAYER_BET = 'player_bet',
+  RESET_BETS = 'reset_bets',
 }
 
 export type StartGameEvent = {
@@ -24,9 +28,22 @@ export type SeatToActEvent = {
   seatToAct: number;
 };
 
+export type UpdatePotEvent = {
+  pot: number;
+};
+export type PlayerBetEvent = {
+  seatNumber: number;
+  betAmount: number;
+  chipCount: number;
+};
+
 export interface GameEvents {
   start_game: (payload: StartGameEvent) => void;
   deal_cards: (payload: DealCardsEvent) => void;
   fold_cards: (payload: FoldCardsEvent) => void;
   seat_to_act: (payload: SeatToActEvent) => void;
+  deal_community_cards: (payload: DealCardsEvent) => void;
+  update_pot: (payload: UpdatePotEvent) => void;
+  player_bet: (payload: PlayerBetEvent) => void;
+  reset_bets: () => void;
 }
