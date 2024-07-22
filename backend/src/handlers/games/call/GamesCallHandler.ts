5cd224fd50360a79a875867b3ff9edd732ed888a
Thomas Frazer <thomasmichaelfrazer@gmail.com>
2024-07-22 23:06:38 +1000

    update endpoints to use action handler

--- backend/src/handlers/games/call/GamesCallHandler.ts
+++ backend/src/handlers/games/call/GamesCallHandler.ts
@@ -1,19 +1,23 @@
 import { ResultError, ResultSuccess } from '@infra/Result';
-import { MethodNotImplementedError } from '@shared/api/BaseOutput';
 import { GamesCallOutput, GamesCallPayload } from '@shared/api/gen/games/types/GamesCall';
 
-import { GameDoesNotExistError } from '../errors/gen/GameDoesNotExistError';
-import { NotPlayersTurnError } from '../errors/gen/NotPlayersTurnError';
-import { PlayerNotFoundAtTableError } from '../errors/gen/PlayerNotFoundAtTableError';
+import { GameLobbyService } from '../../../game-lobby-service';
+import { Dealer } from '../../../game/Dealer';
 import { PokerTableDoesNotExistError } from '../errors/gen/PokerTableDoesNotExistError';
 import { AbstractGamesCallHandler } from './gen/AbstractGamesCallHandler';
 
 export class GamesCallHandler extends AbstractGamesCallHandler {
-  protected async getResult(payload: GamesCallPayload) {
-    return new ResultError(new MethodNotImplementedError());
+  protected async getResult(payload: GamesCallPayload, userId: number) {
+    const pokerTable = GameLobbyService.getPokerTable(payload.pokerTableName);
+    if (!pokerTable) {
+      return new ResultError(new PokerTableDoesNotExistError());
+    }
 
-    // 1. After generating the handler, create a PR returning the above
-    // 2. Then implement the handler when the above PR is merged and use the below
-    // return new ResultSuccess<GamesCallOutput>();
+    const foldPlayer = Dealer.actionHandler(pokerTable, 'call', 0, userId);
+    if (foldPlayer.isError()) {
+      return new ResultError(foldPlayer.getError());
+    }
+
+    return new ResultSuccess<GamesCallOutput>({ ok: true });
   }
 }
--- backend/src/handlers/games/check/GamesCheckHandler.ts
+++ backend/src/handlers/games/check/GamesCheckHandler.ts
@@ -1,13 +1,22 @@
 import { ResultError, ResultSuccess } from '@infra/Result';
 import { GamesCheckOutput, GamesCheckPayload } from '@shared/api/gen/games/types/GamesCheck';
 
-import { NotPlayersTurnError } from '../errors/gen/NotPlayersTurnError';
-import { PlayerNotFoundAtTableError } from '../errors/gen/PlayerNotFoundAtTableError';
+import { GameLobbyService } from '../../../game-lobby-service';
+import { Dealer } from '../../../game/Dealer';
 import { PokerTableDoesNotExistError } from '../errors/gen/PokerTableDoesNotExistError';
 import { AbstractGamesCheckHandler } from './gen/AbstractGamesCheckHandler';
 
 export class GamesCheckHandler extends AbstractGamesCheckHandler {
-  protected async getResult(payload: GamesCheckPayload) {
+  protected async getResult(payload: GamesCheckPayload, userId: number) {
+    const pokerTable = GameLobbyService.getPokerTable(payload.pokerTableName);
+    if (!pokerTable) {
+      return new ResultError(new PokerTableDoesNotExistError());
+    }
+
+    const foldPlayer = Dealer.actionHandler(pokerTable, 'check', 0, userId);
+    if (foldPlayer.isError()) {
+      return new ResultError(foldPlayer.getError());
+    }
     return new ResultSuccess<GamesCheckOutput>({ ok: true });
   }
 }
